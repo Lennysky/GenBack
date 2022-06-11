7e38eb0c384ad200e1362818eeac88c8d9fb07fe
Lennysky <ekhinelly@gmail.com>
2022-06-11 21:44:26 +0300

    added server and 2 html-pages

---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require('http')
+
+
+const server = http.createServer((request, response) => {
+    switch (request.url) {
+        case '/home': {
+            const data = 'best free online course'
+            response.write(data)
+            break;
+        }
+        default: {
+            response.write('404: not found')
+
+        }
+    }
+    response.end()
+})
+server.listen(3003)
